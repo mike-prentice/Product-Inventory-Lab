134f0b59b574b8f9d6cd61c74f5887aef66dc4ef
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-26 18:24:41 -0400

    sneaker tests

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -6,4 +6,44 @@ public class Whiskey {
     private int size;
     private float price;
     private int rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
--- src/test/java/models/WhiskeyTest.java
+++ src/test/java/models/WhiskeyTest.java
@@ -1,4 +1,45 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 public class WhiskeyTest {
+    @Test
+    void getNameTest() {
+    }
+
+    @Test
+    void setNameTest() {
+    }
+
+    @Test
+    void getTypeTest() {
+    }
+
+    @Test
+    void setTypeTest() {
+    }
+
+    @Test
+    void getSizeTest() {
+    }
+
+    @Test
+    void setSizeTest() {
+    }
+
+    @Test
+    void getPriceTest() {
+    }
+
+    @Test
+    void setPriceTest() {
+    }
+
+    @Test
+    void getRatingTest() {
+    }
+
+    @Test
+    void setRatingTest() {
+    }
 }
