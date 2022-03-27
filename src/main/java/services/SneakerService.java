eddce91a462ea83df89a0e6e6fca3ca1499973a4
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-26 21:35:01 -0400

    findSneaker and test

--- src/main/java/services/SneakerService.java
+++ src/main/java/services/SneakerService.java
@@ -1,4 +1,29 @@
 package services;
 
+import models.Sneaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SneakerService {
+
+    private static int nextId = 1;  // (1)
+
+    private static List<Sneaker> inventory = new ArrayList<>();
+
+    public static Sneaker create(String name, String brand,
+                                 String sport, int size,
+                                 int qty, float price) {
+        Sneaker createdSneaker = new Sneaker(nextId++, name, brand, sport, size, qty, price);
+        inventory.add(createdSneaker);
+        return createdSneaker;
+    }
+
+    public static Sneaker findSneaker(int id){
+        return inventory.get(id);
+    }
+
+    public boolean Sneaker deleteSneaker(int id){
+        inventory.remove(id);
+    }
 }
